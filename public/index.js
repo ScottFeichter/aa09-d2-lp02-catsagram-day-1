7e8028342f6be9e4c31fcbcd4c9d8834b84a916d
Scott Feichter <scottfeichter@gmail.com>
2024-04-18 13:50:50 -0700

    situated

--- public/index.js
+++ public/index.js
@@ -1,47 +1,86 @@
 console.log(`CONNECTION TEST`);
 
-const createHeader = (content) => {
-  const headerH1 = document.createElement("h1");
-  headerH1.innerText = content;
-  headerH1.setAttribute("style", "font-size: 400px");
-
-  const header = document.createElement("header");
-  header.appendChild(headerH1);
-  // why is my css not happening on the header?
-  return header;
-};
-
-// const createCatContainer = (img) => {
-//   const catContainer = document.createElement("div");
-//   catContainer.setAttribute("style", "text-align: center");
-//   catContainer.appendChild(img);
-//   return catContainer;
-// };
-
-const createButtons = (si, no) => {
-  const buttonContainer = document.createElement("div");
-
-  const yes = document.createElement("button");
-  yes.innerText = si;
-
-  const nay = document.createElement("button");
-  nay.innerText = no;
-
-  buttonContainer.appendChild(yes);
-  buttonContainer.appendChild(nay);
-  return buttonContainer;
-};
-
 // ==================  DOM Content Loaded ========================
 
 document.addEventListener(`DOMContentLoaded`, () => {
   const body = document.body;
 
   const header = createHeader("kitties!");
-  // const catContainer = createCatContainer("image");
-  const buttonContainer = createButtons("purrr", "hisss");
+  const catContainer = createCatContainer();
+  const controlsContainer = createControlsContainer();
 
   body.appendChild(header);
-  // body.appendChild(catContainer);
-  body.appendChild(buttonContainer);
+  body.appendChild(catContainer);
+  body.appendChild(controlsContainer);
+
+  console.log("DOMContentLoaded");
 });
+
+// ==================  Header ========================
+
+const createHeader = (content) => {
+  const headerH1 = document.createElement("h1");
+  headerH1.innerText = content;
+
+  const header = document.createElement("header");
+  header.appendChild(headerH1);
+
+  return header;
+};
+
+// ==================  catContainer ========================
+
+const createCatContainer = (img) => {
+  const catContainer = document.createElement("div");
+  catContainer.setAttribute("id", "catContainer");
+  // catContainer.appendChild(img);
+  return catContainer;
+};
+
+// ==================  controlsContainer ========================
+
+const createControlsContainer = () => {
+  const controlsContainer = document.createElement("div");
+  controlsContainer.setAttribute("id", "controlsContainer");
+
+  const newCatButtonContainer = createNewCatButton();
+  controlsContainer.appendChild(newCatButtonContainer);
+
+  const voteButtonsContainer = createVoteButtons(`purrr`, `hisss`);
+  controlsContainer.appendChild(voteButtonsContainer);
+
+  return controlsContainer;
+};
+
+const createNewCatButton = () => {
+  const newCatButtonContainer = document.createElement("div");
+  newCatButtonContainer.setAttribute("id", "newCatButtonContainer");
+
+  const newCatButton = document.createElement("button");
+  newCatButton.setAttribute("id", "newCatButton");
+  newCatButton.innerText = "new kitten";
+
+  newCatButtonContainer.appendChild(newCatButton);
+  return newCatButtonContainer;
+};
+
+const createVoteButtons = (yes, no) => {
+  const voteButtonsContainer = document.createElement("div");
+  voteButtonsContainer.setAttribute(`id`, `voteButtonsContainer`);
+
+  const purrr = document.createElement("button");
+  purrr.setAttribute("class", "voteButton");
+  purrr.innerText = yes;
+
+  const hisss = document.createElement("button");
+  hisss.setAttribute("class", "voteButton");
+  hisss.innerText = no;
+
+  voteButtonsContainer.appendChild(purrr);
+  voteButtonsContainer.appendChild(hisss);
+  return voteButtonsContainer;
+};
+
+const createCommentsBox = () => {
+
+}
